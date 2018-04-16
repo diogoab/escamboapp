4336029ee5b3f6afb2a38a277cd4c70233e00def
diogoab <diogo.alves.barbosa@gmail.com>
2018-04-15 23:27:57 -0300

    config scope divises

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   BetterErrors::Middleware.allow_ip! '181.222.232.252'
+
+  config.action_mailer.default_url_options = { host: '0.0.0.0', port: 3000 }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
